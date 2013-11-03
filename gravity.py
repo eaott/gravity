d2edfb313eb07ea4aed95e2c1aabf6785144f796
Evan Ott <evan.ott@utexas.edu>
2013-11-03 12:51:20 -0600

    done

--- gravity.py
+++ gravity.py
@@ -10,7 +10,7 @@ avg_time = sum(times)/len(times)
 avg_g = 2 * HEIGHT / (avg_time ** 2)
 std_time = ((1.0/(len(times) - 1) * sum(map(lambda x: (x - avg_time) ** 2, times)))) ** (0.5)
 std_g = ((4 * HEIGHT / (avg_time ** 3)) ** 2 * (std_time ** 2)) ** (0.5)
-print "RLM Height:",HEIGHT
-print "avg_time:",avg_time
-print "std_time:",std_time
-print "g:",avg_g,"+/-",std_g
+print "RLM Height:",HEIGHT,"m"
+print "avg_time:",avg_time,"s"
+print "std_time:",std_time,"s"
+print "g:",avg_g,"+/-",std_g,"m/s^2"
